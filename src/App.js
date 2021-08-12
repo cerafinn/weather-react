fad17095de12e170ffebfd3ad4c6eb5c7481d60d
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-08-12 14:43:38 -0400

    changes to file structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
-import Footer from './pages/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
